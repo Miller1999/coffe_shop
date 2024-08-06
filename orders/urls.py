a0aff6ab36ec33f257538ede092bf4d2ec61f6fb
Miller Arias <millerjavierarias@gmail.com>
2024-08-06 14:24:10 -0500

    my-order

--- orders/urls.py
+++ orders/urls.py
@@ -1,1 +1,5 @@
-urlpatterns = []
+from django.urls import path
+from .views import MyOrderView
+
+
+urlpatterns = [path("my-order", MyOrderView.as_view(), name="my_order")]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
+from django.views.generic import DetailView
+
+from .models import Order
 
 # Create your views here.
+
+
+class MyOrderView(DetailView):
+    model = Order
+    template_name = "orders/my_order.html"
+
+    def get_object(self, queryset=None) -> Model:
+        return Order.objects.filter(is_active=True).first()
