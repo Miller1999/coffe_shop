4072c12cc9c2d487d25fa574a4c3255df0a88341
Miller Arias <millerjavierarias@gmail.com>
2024-08-06 23:45:37 -0500

    Postgres en Render

--- coffe_shop/settings.py
+++ coffe_shop/settings.py
@@ -12,10 +12,14 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Se instala DJANGO-ENVIRON para usar variables de entorno, se crea el .env y se usa asi:
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -81,12 +85,7 @@ WSGI_APPLICATION = "coffe_shop.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": env.db("DJANGO_DB_URL")}
 
 
 # Password validation
